18f6824e89fbc96509453966e19e200a584ffbd9
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-05-13 23:35:06 +0300

    feat: add error and spiner for obj

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -1,7 +1,9 @@
 import { Component } from 'react';
+import Spinner from '../spinner/Spinner';
+import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
+
 import './randomChar.scss';
-import Spinner from '../spinner/spinner';
 import mjolnir from '../../resources/img/mjolnir.png';
 
 class RandomChar extends Component {
@@ -42,12 +44,16 @@ class RandomChar extends Component {
 
 
     render() {
-        const {char, loading} = this.state;
-
+        const {char, loading, error} = this.state;
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner = loading ? <Spinner/> : null;
+        const content = !(loading || error) ? <View char={char}/> : null
 
         return (
             <div className="randomchar">
-                {loading ? <Spinner/> : <View char={char}/>}
+                {errorMessage}
+                {spinner}
+                {content}
                 <div className="randomchar__static">
                     <p className="randomchar__title">
                         Random character for today!<br/>
