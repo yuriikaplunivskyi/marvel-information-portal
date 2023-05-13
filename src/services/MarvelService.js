dbeb0c396822fc22dd76d4511c991ff867831d60
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-05-13 12:48:21 +0300

    feat: add request to API

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -3,6 +3,7 @@
 
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    //me personal API-key
     _apiKey = 'apikey=a127afaa36dcb5288084158f306b15ee';
 
     getResource = async (url) => {
@@ -15,11 +16,22 @@ class MarvelService {
     }
 
     getAllCharacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=1000&${this._apiKey}`);
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
     }
 
-    getCharacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (res) => {
+      return  {
+        name: res.data.results[0].name,
+        description: res.data.results[0].description,
+        thumbnail: res.data.results[0].thumbnail.path + '.' + res.data.results[0].thumbnail.extension,
+        homepage: res.data.results[0].urls[0].url,
+        wiki: res.data.results[0].urls[1].url
+      }
     }
 }
 
