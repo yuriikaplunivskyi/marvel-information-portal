5e9965213388e09a6b2ea96ec3c2c986814e6eb3
yuriikaplunivskyi <yurii.kaplunivskyi@gmail.com>
2023-05-14 22:17:43 +0300

    feat: add render grid char from api

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -8,6 +8,7 @@ class MarvelService {
 
     getResource = async (url) => {
         let res = await fetch(url);
+
         if ( !res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
@@ -27,6 +28,7 @@ class MarvelService {
 
     _transformCharacter = (char) => {
       return  {
+        id: char.id,
         name: char.name,
         description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
         thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
